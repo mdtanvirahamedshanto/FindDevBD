639f50721da47a1728cc36a76393a618d5d17a10
Md Tanvir Ahamed Shanto <mdtanvirahamdsanto987@gmail.com>
2023-12-28 13:38:57 +0600

    Push All

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './Sidebar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCode, faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import useTheme from '../../hooks/useTheme';
+
+function Sidebar() {
+  const { theme, toogleTheme } = useTheme();
+  return (
+    <div className="sidebar">
+      <div className="header">
+        <div className="logo">
+          <FontAwesomeIcon icon={faCode} size="2xl" />
+        </div>
+        <a href="https://dev-find.vercel.app/" className="titlelink">
+          <div className="title">
+            <p className="dev">dev</p>
+            <p className="find">Find</p>
+          </div>
+        </a>
+        <div className="theme">
+          <button type="button" className="theme-button" onClick={toogleTheme}>
+            {theme === 'light' ? (
+              <FontAwesomeIcon icon={faMoon} fontSize="1rem" />
+            ) : (
+              <FontAwesomeIcon icon={faSun} fontSize="1rem" />
+            )}
+          </button>
+        </div>
+      </div>
+      <div className="headline">Discover and Connect with Skilled Developers.</div>
+      <div className="description">
+        <a href=" https://github.com/shyamtawli/devFind#how-to-add-your-profile-" target="_blank" rel="noreferrer">
+          <button className="link_button"> Add your profile </button>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
